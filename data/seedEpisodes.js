2a8600baa154386393f9dc55fd260faf58c1f4e6
Ren-mayday <rencel.dayrit@gmail.com>
2025-10-06 20:26:37 +0200

    added duration to episodeSchema. Created data folder, inside it contains seedEpisodes.js that has episodes array of objects.  Created utils > Seeds > episodes.seeds.js where we will connect to BBDD and insert the array of episodes object. Finally on package.json added to "scripts" the following: "episodes.seeds": "node ./utils/Seeds/episodes.seeds.js"

---
+++ data/seedEpisodes.js
@@ -0,0 +1,97 @@
+//Array de objetos con el mismo formato que el esquema de episodeSchema
+
+const episodes = [
+  {
+    title: "Carne humana",
+    season: 1,
+    episodeNumber: 1,
+    duration: 22,
+    description: `Bob debe lidiar con un inspector de sanidad que visita el restaurante
+tras escuchar un rumor iniciado por Louise de que las hamburguesas están hechas de carne humana.`,
+    characters: [],
+  },
+  {
+    title: "Espacio en la pared",
+    season: 1,
+    episodeNumber: 2,
+    duration: 22,
+    description: `Bob teme la visita de la madre de Linda. Mientras Linda intenta mantener el apartamento impecable
+y le pide a Bob que repare una gotera en el techo, él queda atrapado dentro de la pared.
+Linda y los niños deben encargarse solos de los visitantes y del restaurante.`,
+    characters: [],
+  },
+  {
+    title: "Vaca sagrada",
+    season: 1,
+    episodeNumber: 3,
+    duration: 22,
+    description: `Los clientes comienzan a evitar el restaurante cuando un polémico documentalista deja una vaca afuera del local.
+El descontento de Bob con la situación se convierte en apego, y empieza a tratar a la vaca como si fuera parte de la familia.`,
+    characters: [],
+  },
+  {
+    title: "Baile de lucha sexy",
+    season: 1,
+    episodeNumber: 4,
+    duration: 22,
+    description: `Tina empieza a tomar clases con el nuevo instructor de artes marciales de la bahía de Seymour,
+pero cuando eso la hace descuidar sus tareas en el restaurante, Bob decide intervenir.`,
+    characters: [],
+  },
+  {
+    title: "Cena con teatro de hamburguesas",
+    season: 1,
+    episodeNumber: 5,
+    duration: 22,
+    description: `Linda y los niños convencen a Bob para organizar un teatro musical de misterio en el restaurante,
+pero algo inesperado sucede la noche del estreno que cambia por completo el ambiente.`,
+    characters: [],
+  },
+  {
+    title: "¡Taxi, Bob?",
+    season: 1,
+    episodeNumber: 6,
+    duration: 22,
+    description: `Tina quiere tener su primer beso en su fiesta de 13 años, y Bob debe conseguir un segundo empleo como taxista nocturno para pagarla.
+Además, debe resolver la situación cuando los padres del chico que le gusta no le permiten asistir.`,
+    characters: [],
+  },
+  {
+    title: "Cama y desayuno",
+    season: 1,
+    episodeNumber: 7,
+    duration: 22,
+    description: `Linda quiere convertir la casa familiar en un bed and breakfast para aprovechar un fin de semana rentable.
+Pero cuando un huésped sospechoso se registra, Louise se propone descubrir qué está ocurriendo realmente.`,
+    characters: [],
+  },
+  {
+    title: "Ruta del arte",
+    season: 1,
+    episodeNumber: 8,
+    duration: 22,
+    description: `El consejo de arte de Seymour’s Bay confronta a Bob después de que permite que Gayle, la hermana de Linda,
+cuelgue sus pinturas en el restaurante durante la Semana del Arte. Mientras tanto, el arte de los niños Belcher también llama la atención.`,
+    characters: [],
+  },
+  {
+    title: "Spaghetti Western y albóndigas",
+    season: 1,
+    episodeNumber: 9,
+    duration: 22,
+    description: `Louise se siente sola cuando Bob y Gene comienzan a ver películas del oeste italiano.
+Las cintas inspiran a Gene a enfrentarse a su rival escolar, mientras Tina aprende a controlar su agresividad en un programa de resolución de conflictos.`,
+    characters: [],
+  },
+  {
+    title: "Guerras de hamburguesas",
+    season: 1,
+    episodeNumber: 10,
+    duration: 22,
+    description: `Los Belcher deben salvar el restaurante después de que su excéntrico casero, el señor Fischoeder,
+le dice a Bob que Jimmy Pesto quiere quedarse con su local para expandir su negocio.`,
+    characters: [],
+  },
+];
+
+module.exports = episodes;
